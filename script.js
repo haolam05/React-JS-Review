bb38f655c550921cff09cec43c8529ae8b435bee
Hao Lam <tuonghao2001@gmail.com>
2023-08-15 09:41:35 -0700

    async/await

--- script.js
+++ script.js
@@ -270,3 +270,12 @@ booksAfterUpdate;
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+// Async/Await
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+getTodos();
+console.log("Hao");
